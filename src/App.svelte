75db90aaa80ff03d0bdaa8b97a84e1fdccd4e001
Manuel Baun <manuel.baun@gmail.com>
2021-01-25 14:31:46 +0100

    fixed colors and font size

--- src/App.svelte
+++ src/App.svelte
@@ -14,10 +14,12 @@
       <Codemirror />
     </div>
   </div>
+  <div class="divider text-center" data-content="Utils"></div>
   <div class="columns">
     <div class="column col-6">
       <Controller />
     </div>
+
     <div class="column col-6">
       <JsonViewer />
     </div>
--- src/components/JsonViewer.svelte
+++ src/components/JsonViewer.svelte
@@ -7,7 +7,7 @@
 
   const {
     progress,
-    speed,
+    animationSpeed,
     state,
     event,
     currentTime,
@@ -27,7 +27,7 @@
       breakPoints: $breakPoints,
       control: {
         progress: $progress.toFixed(2) + " %",
-        speed: $speed,
+        speed: $animationSpeed.toFixed(2),
         state: $state,
         event: $event,
         currentTime: +$currentTime.toFixed(2) + " ms",
@@ -35,7 +35,29 @@
     };
   }
   // autosubscribe and listen to data changes
-  $: $progress, $speed, $state, $event, $breakPoints, jsonData();
+  $: $progress, $animationSpeed, $state, $event, $breakPoints, jsonData();
 </script>
 
 <json-viewer id="json" {data} bind:this={jsonViewer} />
+
+<style>
+  :global(json-viewer) {
+    --background-color: #282a36;
+    --color: #f8f8f2;
+    --font-family: monaco, Consolas, "Lucida Console", monospace;
+    font-size: 1em;
+
+    /* Types colors */
+    --string-color: #7bff82;
+    --number-color: #66d4ff;
+    --boolean-color: #fa5fff;
+    --null-color: #dd6dff;
+    --property-color: #fffffa;
+
+    /* Collapsed node preview */
+    --preview-color: rgba(135, 245, 120, 0.9);
+
+    /* Search highlight color */
+    --highlight-color: #6fb3d2;
+  }
+</style>
